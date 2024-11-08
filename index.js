96ba1fd56cf937253325e8d0f3a74ae0dc1337b9
Your Name <you@example.com>
2024-11-08 15:31:32 -0500

    new

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-var randomNum = 321; // test number
-var clock = 7;
-var digit;
+var codeInput = "";
+var code = "321";
+var time = 7;
 
-function addDigit() {
-    digit = prompt("Enter a number");
+
+function addOne() {
+    codeInput += "1";
+    checkIt();
+}
+
+function addTwo() {
+    codeInput += "2";
+    checkIt();
+}
+
+function addThree() {
+    codeInput += "3";
+    checkIt();
     
 }
 
+function checkIt() {
+    if(codeInput.length == 3) {
+        if(codeInput === code) {
+            document.getElementById("message").innerText = "You cracked the code!";
+        } else if(Number(code) > Number(codeInput)) {
+            document.getElementById("message").innerText = "Incorrect code, try a higher number";
+        } else {
+            document.getElementById("message").innerText = "Incorrect code, try a lower number";
+        }
+        codeInput = "";
+        time -= 1;
+        document.getElementById("clock").innerText = "Clock: " + time;
+    } 
+    document.getElementById("number").innerText = codeInput;
+}
