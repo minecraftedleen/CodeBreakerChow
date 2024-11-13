f1cc8e95f60c9c9024f0127abbfefed21da4436e
Your Name <you@example.com>
2024-11-13 15:30:23 -0500

    yes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var codeInput = "";
-var code = "321";
+var code = "";
 var time = 7;
+var win = false;
 
+function addHighScore() {
+    
+}
 
 function addOne() {
     codeInput += "1";
@@ -16,13 +20,34 @@ function addTwo() {
 function addThree() {
     codeInput += "3";
     checkIt();
-    
+}
+
+function generateCode() {
+    code = "";
+    for (i = 0; i < 3; i++) {
+        code += Math.round(Math.random() * 2 + 1);
+    }
+    alert("Uh oh, you've tripped an alarm!");
+}
+
+function clearGame() {
+    codeInput = "";
+    time = 7;
+    document.getElementById("number").innerText = codeInput;
+    document.getElementById("clock").innerText = "Clock: " + time;
+    document.getElementById("message").setAttribute("style", "color:black");
+    document.getElementById("message").innerText = "Game reset, Crack the code!";
+    document.getElementById("b1").disabled = false;
+    document.getElementById("b2").disabled = false;
+    document.getElementById("b3").disabled = false;
+    var win = false;
+    generateCode();
 }
 
 function checkIt() {
     if(codeInput.length == 3) {
         if(codeInput === code) {
-            document.getElementById("message").innerText = "You cracked the code!";
+            var win = true;
         } else if(Number(code) > Number(codeInput)) {
             document.getElementById("message").innerText = "Incorrect code, try a higher number";
         } else {
@@ -30,7 +55,41 @@ function checkIt() {
         }
         codeInput = "";
         time -= 1;
+        randomChance();
         document.getElementById("clock").innerText = "Clock: " + time;
     } 
     document.getElementById("number").innerText = codeInput;
+    
+    if(time == 0) {
+        document.getElementById("b1").disabled = true;
+        document.getElementById("b2").disabled = true;
+        document.getElementById("b3").disabled = true;
+        document.getElementById("message").innerText = "You've been busted! Game Over \n Press Clear to restart";
+        document.getElementById("message").setAttribute("style", "color:red");
+    }
+    if (win == true) {
+        document.getElementById("b1").disabled = true;
+        document.getElementById("b2").disabled = true;
+        document.getElementById("b3").disabled = true;
+        document.getElementById("message").innerText = "You've cracked the code! \n Press Clear to restart"
+        document.getElementById("message").setAttribute("style", "color:darkgreen");
+    }
+}
+
+function randomChance() {
+    if (win == false) {
+        var rand = Math.random() * 100 + 1;
+        if (rand < 15) {
+            document.getElementById("message").innerText += "\nThe police are caught in a traffic jam, +1 time"
+            time += 1;
+        } else if (rand < 10) {
+            document.getElementById("message").innerText += "\nThe police are in the area, -1 time"
+            time -= 1;
+        }
+        else if (rand < 5) {
+            document.getElementById("message").innerText += "\nYour associates created a distraction, +3 time"
+            time += 3;
+        }
+    }
+   
 }
